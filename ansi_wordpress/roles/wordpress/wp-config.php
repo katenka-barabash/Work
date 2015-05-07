192a9d61bef1bff0b801059ab3fd9b9cc3e2e815
unknown <Kateryna_Barabash@EPUAKYIW1373.kyiv.epam.com>
2015-05-07 16:04:32 +0300

    Add ansible playbook to install wordpress with lamp

---
+++ ansi_wordpress/roles/wordpress/wp-config.php
@@ -0,0 +1,9 @@
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'wordpress');
+
+/** MySQL database username */
+define('DB_USER', 'wordpressuser');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'password');
