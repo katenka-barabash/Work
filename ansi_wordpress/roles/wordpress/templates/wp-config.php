a1c3ae028d3020b12cbf528135f155cef05c693a
Kateryna <kate@kateryna_barabash@epam.com>
2015-05-12 15:06:35 +0300

    Many tries to install wordpress

--- ansi_wordpress/roles/wordpress/templates/wp-config.php
+++ ansi_wordpress/roles/wordpress/templates/wp-config.php
@@ -1,3 +1,5 @@
+<?php
+
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define('DB_NAME', 'wordpress');
@@ -7,3 +9,12 @@ define('DB_USER', 'wordpressuser');
 
 /** MySQL database password */
 define('DB_PASSWORD', 'password');
+
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+        define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
+                                                                              
